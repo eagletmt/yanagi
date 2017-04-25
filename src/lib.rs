bf2c681c34fc2b847d4948558d622113089380b7
Kohei Suzuki <eagletmt@gmail.com>
2017-04-26 03:57:53 +0900

    Add signalfd

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
+pub mod signalfd;
+pub use signalfd::SignalFd;
+
 mod timerfd;
 pub use timerfd::TimerFd;
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate time;
 extern crate yanagi;
 
 fn main() {
+    {
+        let signalfd =
+            yanagi::SignalFd::new(&[yanagi::signalfd::SIGINT]).expect("Unable to create signalfd");
+        println!("{:?}", signalfd);
+        let ssi = signalfd.read().expect("Unable to read signalfd");
+        match ssi.ssi_signo as i32 {
+            yanagi::signalfd::SIGINT => {
+                println!("Got SIGINT");
+            }
+            signum => panic!("Unexpected ssi_signo {}", signum),
+        }
+    }
     let target = chrono::Local::now() + time::Duration::seconds(3);
     let timerfd = yanagi::TimerFd::new(target).expect("Unable to create timerfd");
     println!("{:?}", timerfd);
