009367463b1e27fb1f2430adc59140376725adee
Kohei Suzuki <eagletmt@gmail.com>
2017-05-03 02:18:32 +0900

    Add channels table

--- src/database.rs
+++ src/database.rs
@@ -22,6 +22,16 @@ pub struct Program {
     pub subtitle: String,
     pub title: String,
     pub comment: String,
+    pub channel_name: String,
+    pub channel_for_recorder: i32,
+}
+
+#[derive(Debug)]
+pub struct Channel {
+    pub id: i32,
+    pub name: String,
+    pub for_recorder: i32,
+    pub for_syoboi: i32,
 }
 
 impl Program {
@@ -35,6 +45,7 @@ impl Program {
         if !self.comment.is_empty() {
             fname.push_str(&format!(" ({})", self.comment));
         }
+        fname.push_str(&format!(" at {}", self.channel_name));
         fname.replace("/", "／")
     }
 }
@@ -49,12 +60,22 @@ impl Database {
     pub fn initialize_tables(&self) -> Result<(), postgres::error::Error> {
         self.conn
             .execute(r#"
+        create table if not exists channels (
+            id serial not null primary key
+            , name varchar(255) not null unique
+            , for_recorder integer not null unique
+            , for_syoboi integer not null unique
+        )
+        "#,
+                     &[])?;
+        self.conn
+            .execute(r#"
         create table if not exists programs (
             pid integer not null primary key
             , tid integer not null
             , st_time timestamp with time zone not null
             , ed_time timestamp with time zone not null
-            , channel_id integer not null
+            , channel_id integer not null references channels (id)
             , count varchar(16) not null
             , st_offset integer not null
             , subtitle varchar(255) not null
@@ -66,7 +87,7 @@ impl Database {
         self.conn
             .execute(r#"
         create table if not exists jobs (
-            pid integer not null primary key
+            pid integer not null primary key references programs (pid)
             , enqueued_at timestamp with time zone not null
             , finished_at timestamp with time zone
             , created_at timestamp with time zone not null
@@ -105,7 +126,11 @@ impl Database {
                 , p.subtitle
                 , p.title
                 , p.comment
-            from programs p
+                , c.name as channel_name
+                , c.for_recorder as channel_for_recorder
+            from
+                programs p
+                inner join channels c on c.id = p.channel_id
             where p.pid = $1
         "#,
                    &[&pid])?;
@@ -123,6 +148,8 @@ impl Database {
                 subtitle: row.get("subtitle"),
                 title: row.get("title"),
                 comment: row.get("comment"),
+                channel_name: row.get("channel_name"),
+                channel_for_recorder: row.get("channel_for_recorder"),
             }
         }))
     }
